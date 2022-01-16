2ba3f4f894cd806b4fd2d36cafad4bec07afdb38
chungmin23 <dlcndals2005@naver.com>
2022-01-16 21:53:09 +0900

    0116 api 추가

--- src/main/java/com/example/demo/src/orderHistory/OrderHistoryDao.java
+++ src/main/java/com/example/demo/src/orderHistory/OrderHistoryDao.java
@@ -1,10 +1,13 @@
 package com.example.demo.src.orderHistory;
 
+import com.example.demo.src.orderHistory.model.GetDealNumberRes;
+import com.example.demo.src.products.model.GetSearchRes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @Repository
 public class OrderHistoryDao {
@@ -15,4 +18,35 @@ public class OrderHistoryDao {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
+    public List<GetDealNumberRes> getDealNumber(int userId){
+        String getContentQuery = "SELECT\n" +
+                "\t(SELECT COUNT(*) FROM ORDER_HISTORY O\n" +
+                "\tWHERE user_id = ? AND order_state = 0\n" +
+                "\tAND type = 'buy'\n" +
+                "\tGROUP BY type) AS buy_bidding\n" +
+                "    ,\n" +
+                "\t(SELECT COUNT(*) FROM ORDER_HISTORY O\n" +
+                "\tWHERE user_id = ? AND order_state = 1\n" +
+                "\tAND type = 'buy'\n" +
+                "\tGROUP BY type) AS buy_end\n" +
+                "    ,\n" +
+                "\t(SELECT COUNT(*) FROM ORDER_HISTORY O\n" +
+                "\tWHERE user_id = ? AND order_state = 0\n" +
+                "\tAND type = 'SELL'\n" +
+                "\tGROUP BY type) AS sell_bidding\n" +
+                "    ,\n" +
+                "\t(SELECT COUNT(*) FROM ORDER_HISTORY O\n" +
+                "\tWHERE user_id = ? AND order_state = 1\n" +
+                "\tAND type = 'SELL'\n" +
+                "\tGROUP BY type) AS sell_end\n";
+        int getSearchFilterParam = userId;
+        return this.jdbcTemplate.query(getContentQuery,
+                (rs,rowNum) -> new GetDealNumberRes(
+                        rs.getInt("buy_bidding"),
+                        rs.getInt("buy_end"),
+                        rs.getInt("sell_bidding"),
+                        rs.getInt("sell_end")
+                ),getSearchFilterParam,getSearchFilterParam,getSearchFilterParam,getSearchFilterParam
+        );
+    }
 }
--- src/main/java/com/example/demo/src/orderHistory/OrderHistoryProvider.java
+++ src/main/java/com/example/demo/src/orderHistory/OrderHistoryProvider.java
@@ -1,10 +1,16 @@
 package com.example.demo.src.orderHistory;
 
+import com.example.demo.config.BaseException;
+import com.example.demo.src.orderHistory.model.GetDealNumberRes;
 import com.example.demo.utils.JwtService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.example.demo.config.BaseResponseStatus.DATABASE_ERROR;
 
 @Service
 public class OrderHistoryProvider {
@@ -20,4 +26,16 @@ public class OrderHistoryProvider {
         this.jwtService = jwtService;
 
     }
+    public List<GetDealNumberRes> getDealNumber(int userId) throws BaseException {
+        try{
+            List<GetDealNumberRes> getDealNumberRes = orderHistoryDao.getDealNumber(userId);
+            return getDealNumberRes;
+        }
+        catch (Exception exception) {
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
 }
+
+
